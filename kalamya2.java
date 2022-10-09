2bb09f6f4a93c3f236a8057dc38279df280acee6
Kalamya <markvor2077@gmail.com>
2022-10-09 19:37:21 +0300

    1 Kalamya on brunch Kalamya

---
+++ kalamya2.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int i = 0;
+
+        System.out.println(Math.pow(a, 3) + " Your heihgt");
+    }
+}
+
+
+
+
