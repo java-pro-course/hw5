e7a98372c9f0ecf410ae6ee8977684cfd94a225b
MaryamSSS <sayhanova-chechnya@mail.ru>
2022-10-09 20:14:28 +0400

    Do.Not.Enter

---
+++ donotenter2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Чувак");
+        String a = in.nextLine();
+        System.out.println("Просили же не входить");
+        a = in.nextLine();
+        System.out.println("Иди-ка ты отсюда");
+    }
+}
